d9507cae16bb622175d11eff88c4f794041c932f
vuca1 <malihavita@gmail.com>
2026-01-27 08:09:01 +0100

    first commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def title(request, title):
+    return render(request, "encyclopedia/title.html", {
+        "title": title,
+        "entry": util.get_entry(title)
+    })
