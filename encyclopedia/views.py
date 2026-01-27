fa1ba4a70117e5222285e002086fba447e70dd13
vuca1 <malihavita@gmail.com>
2026-01-27 09:58:18 +0100

    Add search functionality and update templates for better navigation

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,8 @@
+import re
+
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
@@ -12,4 +16,22 @@ def title(request, title):
     return render(request, "encyclopedia/title.html", {
         "title": title,
         "entry": util.get_entry(title)
+    })
+
+def search(request):
+    entries = util.list_entries()
+    query = request.GET.get("q")
+
+    for entry in entries:
+        if entry.lower() == query.lower():
+            return HttpResponseRedirect(reverse("title", args=(entry,)))
+        
+    finds = []
+    for entry in entries:
+        if query.lower() in entry.lower():
+            finds.append(entry)
+
+    return render(request, "encyclopedia/index.html", {
+        "entries": finds,
+        "query": query
     })
